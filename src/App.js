053a7a2a8e782170f64285ecfcc3e5d1c5b21c93
Manoj M <manojmanu5401@gmail.com>
2023-11-06 22:29:00 +0530

    add vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
    <>
+   <Analytics />
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
